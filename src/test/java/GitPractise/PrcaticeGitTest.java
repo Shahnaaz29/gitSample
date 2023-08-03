ea9fec23ee3515f1916b9e0dd2aebc3d7b24b6cd
Shannu <santra.shaik29@gmail.com>
2023-08-03 19:52:46 +0530

    demo is committed

---
+++ src/test/java/GitPractise/PrcaticeGitTest.java
@@ -0,0 +1,12 @@
+package GitPractise;
+
+import org.testng.annotations.Test;
+
+public class PrcaticeGitTest {
+@Test 
+public void demo()
+{
+	System.out.println("Hi its my first commit");
+	
+}
+}
